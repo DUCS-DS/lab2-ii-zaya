8c4933f2eb348b983a6d408ab42d47b8089d9972
ii-zaya <isaiahblittle@gmail.com>
2025-01-29 16:03:50 -0600

    finished monotonic function & edit readme

--- edit_me.py
+++ edit_me.py
@@ -1,11 +1,28 @@
 
 def monotonic(lst):
     """Return True if lst is monotonic; return False, otherwise."""
-    #
-    # YOUR IMPLEMENTATION GOES HERE
-    #
 
-#
-# Feel free to replace these comments with
-# code that tests your function monotonic.
-#
+    inc = True
+    dec = True 
+    firstnum = lst[0]
+
+    for num in lst[1:]:
+        if firstnum > num:
+            inc = False     
+        else:
+            dec = False
+        firstnum = num 
+
+    if inc or dec:
+        return True
+    else:
+        return False 
+  
+test1 = [0, 1, 2, 3]
+test2 = [1,1,3,100]
+test3 = [11, 1,-9,-10]
+test4 = [2,3,2,3]
+print(monotonic(test1))
+print(monotonic(test2))
+print(monotonic(test3))
+print(monotonic(test4))
